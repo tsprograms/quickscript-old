f477b7af9b57fb58c2faaf9c16f6b2a1df769551
tsprograms <tsgithub@gmail.com>
2016-04-17 16:42:16 -0400

    Finish tokenize function

--- src.js
+++ src.js
@@ -8,8 +8,8 @@ Copyright © 2016 TSPrograms.
   // canBeOperator returns whether a string is a valid operator name
   var canBeOperator = function(str) {
     // A string can be an operator if it does not contain alphanumeric characters or whitespace
-    return !(/[a-z0-9\s]/i).test(str + '');
-  }
+    return !(/[a-z0-9_"'\s]/i).test(str + '');
+  };
   
   // tokenize takes a code string and returns an array of "tokens" (not hierarchical yet)
   var tokenize = function(code) {
@@ -60,10 +60,45 @@ Copyright © 2016 TSPrograms.
             if ((/\s/).test(char)) {
               type = 'space';
             }
-            // TODO
+            else if (char === '"' || char === "'") {
+              type = 'string';
+              start = char;
+            }
+            else {
+              type = 'name';
+            }
           }
           break;
-        // TODO
+        case 'string':
+          tokenized[i] += char;
+          if (char === start && code.charAt(i - 1) !== '\\') {
+            ++index;
+            tokenized[index] = '';
+            type = 'space';
+          }
+          break;
+        case 'name':
+          if ((/\s/).test(char)) {
+            type = 'space';
+            ++index;
+            tokenized[index] = char;
+          }
+          else if (char === '"' || char === "'") {
+            type = 'string';
+            start = char;
+            ++index;
+            tokenized[index] = char;
+          }
+          else if (canBeOperator(char)) {
+            type = 'operator';
+            ++index;
+            tokenized[index] = char;
+          }
+          else {
+            tokenized[index] += char;
+          }
+        default:
+          throw 'QuickScript: ParseError: Unknown token type "' + type + '"';
       }
     }
   };
